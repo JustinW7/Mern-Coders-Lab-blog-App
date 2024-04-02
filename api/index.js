b4a2776d3cfc262f9567e098f7ac04b8b41a23ed
Justin <justinvarghesemicheal@gmail.com>
2024-04-02 21:50:13 +0530

    error solved , of post ie env should be on root  and post sign up is successful

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import  dotenv from  "dotenv";
 
 dotenv.config();
 
@@ -14,9 +16,12 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use(express.json());
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
 
 app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
