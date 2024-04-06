ca04ef12804194150018f1e0e49fb78be85c399e
Justin <justinvarghesemicheal@gmail.com>
2024-04-06 21:08:54 +0530

    updated the dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,29 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import SignUp from './pages/SignUp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
-import Home from './pages/Home';
-import About from './pages/About';
+import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
-
-
-
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
- <BrowserRouter>
- <Header/>
- <Routes>
-<Route path="/" element={<Home />}/>
-<Route path="/sign-in" element={<SignIn />}/>
-<Route path="/sign-up" element={<SignUp />}/>
-<Route path="dashboard" element={<Dashboard/>}/>
-<Route path="projects" element={<Projects/>}/>
-<Route path="about" element={<About/>}/>
-
- </Routes>
-
-<Footer/>
- 
- </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
-
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
+  const [tab, setTab] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <div className="">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* Profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
+  );
 }
