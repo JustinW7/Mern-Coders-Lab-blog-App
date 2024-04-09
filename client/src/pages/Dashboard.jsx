1922f34e5c7839b997ec16204813a78089dd503c
Justin <justinvarghesemicheal@gmail.com>
2024-04-09 12:12:16 +0530

    updated all errors and added to the final stage with 3 err

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
-  const [tab, setTab] = useState('');
   const location = useLocation();
-
+  const [tab, setTab] = useState('');
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get('tab');
@@ -14,15 +17,22 @@ export default function Dashboard() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
-    <div>
-      <div className="">
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
         {/* Sidebar */}
         <DashSidebar />
       </div>
-      {/* Profile */}
+      {/* profile... */}
       {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
